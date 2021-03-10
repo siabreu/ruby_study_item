5ebb4c445b44c1592c41395384ff6ff3855211a2
Simone de Abreu <siabreu@gmail.com>
2021-03-10 12:36:37 -0300

    Adiciona study_item#show

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/', to: 'home#index' # controller e action 
+  root to: 'home#index' 
+  
+  resources :study_items, only: [:show]
+end
